d9581d07bb669f7bedc34baa7013ef9b3d32a4db
polidude <793578@seq.org>
2016-09-16 08:48:43 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -3,17 +3,51 @@
  * Write a description of class BallRunner here.
  * 
  * @author (Iain McDowall) 
- * @version (a version number or a date)
+ * @version (Lab 1)
  */
 public class BallRunner
 {
-    public static void run(){
-        BallWorld ballWorld = new BallWorld(200,200);
-        TGPoint tgPoint = new TGPoint(0,0);
-        //public BallBot(BallWorld ballWorld, TGPoint startPoint, double startHeading, int radius)
-        BallBot ballBot = new BallBot(ballWorld, tgPoint, 90.0, 30);
-        while (ballBot.canMoveForward()){
-        }
+    private BallWorld ballWorld;
+    private TGPoint entrancePoint;
+    private BallBot[] ballBotArray;
 
+    public static void run(){
+        BallWorld ballWorld = new BallWorld(500,500);
+        //TGPoint tgPoint = new TGPoint(0,0);
+        //public BallBot(BallWorld ballWorld, TGPoint startPoint, double startHeading, int radius)
+        //BallBot ballBot = new BallBot(ballWorld, tgPoint, 180.0, 30);
+
+        while (true){
+            if (ballBot.canMoveForward(ballWorld)){
+                ballBot.moveForward();
+            }
+            else
+            {
+                ballBot.setHeading(ballBot.getHeading() + 90);
+                ballBot.moveForward();
+
+            }
+
+        }
+    }
+
+    public BallRunner(BallWorld ballWorld1,TGPoint entrancePoint1,int ballBotArrayLength ){
+        ballWorld = ballWorld1;
+        entrancePoint = entrancePoint1;
+        ballBotArray = new BallBot[ballBotArrayLength];
+
+    }
+
+    public int findFreeBallBotIndex(){
+        for(int i = 0; i < ballBotArray.length; i++){
+            if (ballBotArray = null){
+                return int i;
+
+            }
+            else
+            {
+                return int ballBotArray.length;
+            }
+        }
     }
 }
---
+++ Project 2 - Ball World/activity1.java
@@ -0,0 +1,29 @@
+
+///**
+ //* Write a description of //class BallRunner here.
+// * 
+// * @author (Iain McDowall) 
+ //* @version (Lab 1)
+// */
+//public class BallRunner
+//{
+ //   public static void run(){
+ //       BallWorld ballWorld = new BallWorld(500,500);
+ //       TGPoint tgPoint = new TGPoint(0,0);
+        //public BallBot(BallWorld ballWorld, TGPoint startPoint, double startHeading, int radius)
+//        BallBot ballBot = new BallBot(ballWorld, tgPoint, 180.0, 30);
+        
+  //      while (true){
+     //       if (ballBot.canMoveForward(ballWorld)){
+ //       /        ballBot.moveForward();
+          //  }
+   //         else
+          //  {
+     //   /        ballBot.setHeading(ballBot.getHeading() + 90);
+      //          ballBot.moveForward();
+
+          //  }
+
+      //  }
+   // }/
+//}
