051beb3e07bc9edb9a80d0f77f9faf4247834d99
polidude <793578@seq.org>
2016-09-20 13:09:41 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -11,13 +11,22 @@ public class BallRunner
     private TGPoint entrancePoint;
     private BallBot[] ballBotArray;
 
-    public static void run(){
-        BallWorld ballWorld = new BallWorld(500,500);
-        //TGPoint tgPoint = new TGPoint(0,0);
-        //public BallBot(BallWorld ballWorld, TGPoint startPoint, double startHeading, int radius)
-        //BallBot ballBot = new BallBot(ballWorld, tgPoint, 180.0, 30);
 
+    public static void run(){
+        int numberOfBots = 7;
+        BallWorld ballWorld = new BallWorld(500,500);
+        TGPoint tgPoint = new TGPoint(0,0);
+        BallRunner br = new BallRunner(ballWorld, tgPoint, numberOfBots);
+
+    }
+    
+    public BallRunner(BallWorld ballWorld1, TGPoint entrancePoint1, int ballBotArrayLength ){
+        ballWorld = ballWorld1;
+        entrancePoint = entrancePoint1;
+        ballBotArray = new BallBot[ballBotArrayLength];
+        loadBallBotArray();
         while (true){
+            
             if (ballBot.canMoveForward(ballWorld)){
                 ballBot.moveForward();
             }
@@ -30,23 +39,22 @@ public class BallRunner
 
         }
     }
-
-    public BallRunner(BallWorld ballWorld1,TGPoint entrancePoint1,int ballBotArrayLength ){
-        ballWorld = ballWorld1;
-        entrancePoint = entrancePoint1;
-        ballBotArray = new BallBot[ballBotArrayLength];
-
+    public void loadBallBotArray(){
+        for(int i = 0; i < numberOfBots; i++ ){
+            int randDirection = (int)(math.random()*360);
+            ballBotArray[i] = new BallBot(ballWorld, tgPoint, randDirection, 20);
+        }
     }
-
+    
     public int findFreeBallBotIndex(){
         for(int i = 0; i < ballBotArray.length; i++){
             if (ballBotArray = null){
-                return int i;
+                return  i;
 
             }
             else
             {
-                return int ballBotArray.length;
+                return  ballBotArray.length;
             }
         }
     }
